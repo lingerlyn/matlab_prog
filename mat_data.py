c49f00a32b3fc8ec01442fcc2358bb1552fae005
Jinyao Yan <jinyao.yan.work@gmail.com>
2017-05-14 01:28:59 -0400

    write a script

--- mat_data.py
+++ mat_data.py
@@ -6,5 +6,5 @@ with open(train_dir + '/data_full.pkl', 'rb') as f:
     data_full = pickle.load(f)
 
 import numpy, scipy.io
-save_filename = os.path.expanduser('~/matlab_code/mydata.mat')
+save_filename = os.path.expanduser('~/code/matlab_prog/mydata.mat')
 scipy.io.savemat(save_filename, mdict={'data': data_full})
